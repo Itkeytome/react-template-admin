82a2e8fcc2bf3b250cab5c8fa38319bb64a3922a
logg_ <csw1392153909@163.com>
2022-07-02 19:23:31 +0800

    加入scss、antd

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { FC } from "react";
 import Index from "../views/home";
+import Login from "../views/login";
 import Exception from "../views/exception";
 
-const App = () => {
+const App: FC = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Index />}></Route>
+        <Route path="/login" element={<Login />}></Route>
         <Route path="/home" element={<Index />}></Route>
         <Route path="/index" element={<Index />}></Route>
         <Route path="*" element={<Exception />}></Route>
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,5 +1,12 @@
+import { Button } from "antd";
+import '../../design/index.scss'
 const Index = () => {
-  return <div>Index</div>;
+  return (
+    <>
+      <div className="root">Index</div>
+      <Button type="primary">Button</Button>
+    </>
+  );
 };
 
-export default Index
+export default Index;
