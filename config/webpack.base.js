e3c561014aa0809b7a11e00f0632f6647e83d419
logg_ <csw1392153909@163.com>
2022-07-04 23:57:50 +0800

    build: 修改webpack

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -1,5 +1,13 @@
 const path = require('path')
-const webpack = require('webpack')
+// const webpack = require('webpack')
 module.exports = {
-  
+  // 入口文件
+  entry: path.join(__dirname, '../src/index.tsx'),
+  // 打包文件出口
+  output: {
+    filename: 'static/js/[name].js', // 每个js输出的名称
+    path: path.join(__dirname, '../dist'), // 打包结果输出路径
+    clean: true, // webpack4需要配置clean-webpack-plugin来删除dist文件,webpack5内置了
+    publicPath: '/' // 打包后文件的公共前缀路径
+  },
 }
