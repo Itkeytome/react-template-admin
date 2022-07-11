774aceb812e47c8c354c2510224765b1684a6614
logg_ <csw1392153909@163.com>
2022-07-12 00:13:06 +0800

    feat: 登录页制作（仅部分）

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,13 @@
 const name: string = 'csw'
+const age: number = 22
+const male: boolean = false
+const undef: undefined = undefined
+const nul: null = null
+const obj: object = {
+  age,
+  male,
+  undef,
+  nul,
+}
+const sym: symbol = Symbol('unique')
 export default name
--- src/views/login/index.tsx
+++ src/views/login/index.tsx
@@ -1,7 +1,13 @@
-import '../../design/style/login.scss'
+import '@/design/style/login.scss'
 
 const Login = () => {
-  return <div className='login'>登录页面</div>;
+  return (
+    <div className="login">
+      <div className="login-box">
+        <div className='login-box__main'>登录窗口</div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
