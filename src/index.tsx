3eeaaea71697a8e700f8af59aaf6c9e80954bd1c
logg_ <csw1392153909@163.com>
2022-07-21 23:20:12 +0800

    style: 测试git校验

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./design/index.scss";
-import App from './router'
+import App from "./router"
 // import App from "./App";
 
 const root = ReactDOM.createRoot(
